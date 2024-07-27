1a44db8bdef83728558e7e14ceb2a049803736b0
jainam-b <jainambagrecha16@gmail.com>
2024-07-27 11:15:25 +0530

    added post request to signup

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,8 +2,8 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { signupInput, signinInput } from "@jainam-b/event-comman/dist";
-import { sign, verify } from "hono/jwt";
-import { setCookie, getCookie } from "hono/cookie";
+import { sign } from "hono/jwt";
+import { setCookie } from "hono/cookie";
 import bcrypt from "bcryptjs";
 
 export const userRouter = new Hono<{
@@ -22,10 +22,9 @@ userRouter.post("/signup", async (c) => {
   const { success } = signupInput.safeParse(body);
   if (!success) {
     c.status(411);
-    return c.json({
-      msg: "Incorrect Inputs",
-    });
+    return c.json({ msg: "Incorrect Inputs" });
   }
+
   const hashedPassword = await bcrypt.hash(body.password, 10);
   try {
     const user = await prisma.user.create({
@@ -33,35 +32,30 @@ userRouter.post("/signup", async (c) => {
         name: body.name,
         email: body.email,
         password: hashedPassword,
-        role:body.role || "user"
+        role: body.role || "user"
       },
     });
+
     const token = await sign(
-      {
-        id: user.id,
-        email: user.email,
-        role: user.role,
-      },
-      c.env.JWT_SECRET
+      { id: user.id, email: user.email, role: user.role },
+      c.env.JWT_SECRET,
+      
     );
 
     setCookie(c, "token", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "strict",
+      secure: false,
+      sameSite: "lax",
     });
+
     return c.json({ message: "SignUp successfully" });
   } catch (e) {
     console.error("Error creating user:", e);
-    c.status(500); // Use a more appropriate status code
+    c.status(500);
     return c.json({ msg: "Internal Server Error" });
   }
 });
 
-
-
-
-// route to login
 userRouter.post("/signin", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
@@ -71,17 +65,14 @@ userRouter.post("/signin", async (c) => {
   const { success } = signinInput.safeParse(body);
   if (!success) {
     c.status(411);
-    return c.json({
-      msg: "Incorrect Inputs",
-    });
+    return c.json({ msg: "Incorrect Inputs" });
   }
 
   try {
     const user = await prisma.user.findUnique({
-      where: {
-        email: body.email,
-      },
+      where: { email: body.email },
     });
+
     if (!user) {
       c.status(401);
       return c.json({ msg: "Invalid email or password" });
@@ -92,24 +83,23 @@ userRouter.post("/signin", async (c) => {
       c.status(401);
       return c.json({ msg: "Invalid email or password" });
     }
+
     const token = await sign(
-      {
-        id: user.id,
-        email: user.email,
-        role: user.role,
-      },
-      c.env.JWT_SECRET
+      { id: user.id, email: user.email, role: user.role },
+      c.env.JWT_SECRET,
+       
     );
 
     setCookie(c, "token", token, {
-      // httpOnly: true,
-      // secure: true,
-      // sameSite: "strict",
+      httpOnly: true,
+      secure:false,
+      sameSite: "lax",
     });
+
     return c.json({ message: "Logged in successfully" });
   } catch (e) {
-    console.error("Error creating user:", e);
-    c.status(500); // Use a more appropriate status code
+    console.error("Error logging in user:", e);
+    c.status(500);
     return c.json({ msg: "Internal Server Error" });
   }
 });
--- frontend/src/components/SignUpAuth.tsx
+++ frontend/src/components/SignUpAuth.tsx
@@ -1,53 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { SignupInput } from "@jainam-b/event-comman/dist";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+import Cookies from 'js-cookie';
 
 const Auth = () => {
+  const [signupInputs, setSignupInputs] = useState<SignupInput>({
+    name: "",
+    email: "",
+    password: "",
+  });
+  
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setSignupInputs((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const sendRequest = async () => {
+    try {
+      const response=await axios.post(`${BACKEND_URL}/api/v1/user/signup`, signupInputs );
+      console.log(response.data);
+      console.log(Cookies.get('token'));
+      // Navigate to another page after successful signup
+      navigate('/signin');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="h-screen flex items-center justify-center bg-[#F8F8FA] ">
-      <div className="p-10 mr-auto ">
-        <div className="text-4xl text-center  font-bold mb-8">
-          Event <span className="text-[#7848F4]">Hive</span>{" "}
+    <div className="h-screen flex items-center justify-center bg-[#F8F8FA]">
+      <div className="p-10 mr-auto">
+        <div className="text-4xl text-center font-bold mb-8">
+          Event <span className="text-[#7848F4]">Hive</span>
         </div>
-        <div className="text-6xl ">Sign Up to Event Hive</div>
-        <div className="">
-          <div className="pt-8 ">
-            <div className="text-xl pb-4">YOUR NAME</div>
-            <input
-              type="text"
-              id="first_name"
-              className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
-              placeholder="John"
-              required
-            />
-          </div>
-          <div className="pt-4">
-            <div className="text-xl pb-4">EMAIL</div>
-            <input
-              type="text"
-              id="first_name"
-              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
-              placeholder="example@example.com"
-              required
-            />
-          </div>
-          <div className="pt-4">
-            <div className="text-xl pb-4"> PASSWORD</div>
-            <input
-              type="text"
-              id="first_name"
-              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
-              placeholder="********"
-              required
-            />
-          </div>
-          <div className="pt-8 flex justify-center">
-            <button
-              type="button"
-              className="   text-white bg-[#7848F4] hover:bg-[#7848F4]/90 focus:ring-4 focus:ring-[#7848F4]/50 focus:outline-none font-medium rounded-lg text-lg px-20 py-2.5 text-center inline-flex items-center dark:focus:ring-[#7848F4]/50 me-4 mb-4 "
-            >
-              Sign Up
-            </button>
-          </div>
-          <div className="mt-6">
+        <div className="text-6xl">Sign Up to Event Hive</div>
+        <div className="pt-8">
+          <div className="text-xl pb-4">YOUR NAME</div>
+          <input
+            type="text"
+            id="name"
+            className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="John"
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className="pt-4">
+          <div className="text-xl pb-4">EMAIL</div>
+          <input
+            type="email"
+            id="email"
+            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="example@example.com"
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className="pt-4">
+          <div className="text-xl pb-4">PASSWORD</div>
+          <input
+            type="password"
+            id="password"
+            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="********"
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className="pt-8 flex justify-center">
+          <button
+            type="button"
+            onClick={sendRequest}
+            className="text-white bg-[#7848F4] hover:bg-[#7848F4]/90 focus:ring-4 focus:ring-[#7848F4]/50 focus:outline-none font-medium rounded-lg text-lg px-20 py-2.5 text-center inline-flex items-center dark:focus:ring-[#7848F4]/50 me-4 mb-4"
+          >
+            Sign Up
+          </button>
+        </div>
+        <div className="mt-6">
           <div className="relative">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300"></div>
@@ -56,7 +92,6 @@ const Auth = () => {
               <span className="px-2 bg-gray-50 text-gray-500">Or</span>
             </div>
           </div>
-
           <div className="mt-6 flex justify-center">
             <button
               type="button"
@@ -67,7 +102,6 @@ const Auth = () => {
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
